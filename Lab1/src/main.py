e1d556f4329a8e0a1b9113dfeece6eb0aef2c33b
KT-afk <kongtat@Live.com>
2022-10-07 12:32:08 +0800

    Q2 updated with my code

--- Lab1/src/main.py
+++ Lab1/src/main.py
@@ -1,12 +1,12 @@
-import Q1
+#import Q1
 import Q2
-import Q3
+#import Q3
 
-print("|| Task 1: Breadth First Search ||")
-Q1.ucs_noenergy('1', '50')
+# print("|| Task 1: Breadth First Search ||")
+# Q1.ucs_noenergy('1', '50')
 
 print("\n|| Task 2: Uniform Cost Search ||\n")
 Q2.ucs('1', '50')
-
-print("\n|| Task 3: A* Search ||\n")
-Q3.updatedAStar('1', '50',1.22)
+#
+# print("\n|| Task 3: A* Search ||\n")
+# Q3.updatedAStar('1', '50',1.22)
